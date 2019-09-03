f82bc765d808d96831ed53d31568291516126a3b
Tyler Giffel <tyler.giffel@gmail.com>
2019-09-03 10:58:35 -0400

    initial committ

---
+++ NoAFK/NoAFK/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace NoAFK
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.txtbxTimer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 165);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(260, 64);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(43, 92);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(68, 13);
+            this.lblTimer.TabIndex = 1;
+            this.lblTimer.Text = "Timer length:";
+            // 
+            // txtbxTimer
+            // 
+            this.txtbxTimer.Location = new System.Drawing.Point(118, 92);
+            this.txtbxTimer.Name = "txtbxTimer";
+            this.txtbxTimer.ReadOnly = true;
+            this.txtbxTimer.Size = new System.Drawing.Size(100, 20);
+            this.txtbxTimer.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtbxTimer);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnStart);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "No Wait in Queue";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.TextBox txtbxTimer;
+    }
+}
+
---
+++ NoAFK/NoAFK/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Timers;
+using WindowsInput;
+
+namespace NoAFK
+{
+    public partial class frmMain : Form
+    {
+        private System.Timers.Timer _clickTimer;
+        private bool _started = false;
+        delegate void SetTextCallback(string text);
+        private Random _randomKey;
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            _clickTimer = new System.Timers.Timer(10000);
+            _clickTimer.Enabled = false;
+            _clickTimer.Elapsed += _clickTimer_Elapsed;
+
+            _randomKey = new Random(4);
+        }
+
+        private void _clickTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _clickTimer.Stop();
+            var random = new Random();
+            int interval = random.Next(1000, 10000) * 60;
+            //int interval = random.Next(1000, 10000);
+
+            TimeSpan t = TimeSpan.FromMilliseconds(interval);
+
+            var humanReadableLength = string.Format("{1:D2}:{2:D2}:{3:D3}",
+                                    t.Hours,
+                                    t.Minutes,
+                                    t.Seconds,
+                                    t.Milliseconds);
+
+            SetTimerText(humanReadableLength);
+
+            SendRandomKeys();
+
+            _clickTimer.Interval = interval;
+
+            _clickTimer.Start();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(_started)
+            {
+                btnStart.Text = "Start";
+                txtbxTimer.Text = "";
+                _clickTimer.Stop();
+                _started = false;
+            }
+            else
+            {
+                btnStart.Text = "Stop";
+                _clickTimer.Start();
+                _started = true;
+                
+            }
+          
+
+        }
+
+        private void SendRandomKeys()
+        {
+            var inputSim = new InputSimulator();
+
+          
+
+            int rand = _randomKey.Next(4);
+
+            var random = new Random();
+            
+
+            switch (rand)
+            {
+                case 0:
+                    inputSim.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_W);
+                    Thread.Sleep(random.Next(250, 2000));
+                    inputSim.Keyboard.KeyUp(WindowsInput.Native.VirtualKeyCode.VK_W);
+                    break;
+                case 1:
+                    inputSim.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_A);
+                    Thread.Sleep(random.Next(250, 2000));
+                    inputSim.Keyboard.KeyUp(WindowsInput.Native.VirtualKeyCode.VK_A);
+                    break;
+                case 2:
+                    inputSim.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_S);
+                    Thread.Sleep(random.Next(250, 2000));
+                    inputSim.Keyboard.KeyUp(WindowsInput.Native.VirtualKeyCode.VK_S);
+                    break;
+                case 3:
+                    inputSim.Keyboard.KeyDown(WindowsInput.Native.VirtualKeyCode.VK_D);
+                    Thread.Sleep(random.Next(250, 2000));
+                    inputSim.Keyboard.KeyUp(WindowsInput.Native.VirtualKeyCode.VK_D);
+                    break;
+            }
+            Thread.Sleep(300);
+            
+            inputSim.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.SPACE);
+        }
+
+        private void SetTimerText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.txtbxTimer.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetTimerText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.txtbxTimer.Text = text;
+            }
+        }
+
+    }
+}
